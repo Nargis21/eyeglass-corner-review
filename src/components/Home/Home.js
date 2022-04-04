d575c15350b2a96b31eeacd42bb7bcc3c0e4fb39
Nargis21 <nargisakther1933@gmail.com>
2022-04-04 12:58:01 +0600

    Initial Commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className="home-title">
+                <h1>EyeGlass Corner</h1>
+                <p>To buying your first glass Eyeglass corner is best for you. It's a Collections of similar eyeglass frames. Find your favorite style.Stay with us!</p>
+            </div>
+            <div className="home-img">
+                <img src="https://www.eyeworksonline.co.uk/wp-content/uploads/blog-offer-eco-clip-on.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink'
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <CustomLink to='/'>Home</CustomLink>
+            <CustomLink to='/review'>Review</CustomLink>
+            <CustomLink to='/dashboard'>Dashboard</CustomLink>
+            <CustomLink to='/blogs'>Blogs</CustomLink>
+            <CustomLink to='/about'>About</CustomLink>
+        </div>
+    );
+};
+
+export default Navbar;
