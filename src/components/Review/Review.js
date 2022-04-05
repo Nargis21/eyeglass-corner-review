2d40c533ba1133334a6b933181c21edf6cd527bd
Nargis21 <nargisakther1933@gmail.com>
2022-04-05 14:24:27 +0600

    Review section added

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
+import ReviewCart from '../ReviewCart/ReviewCart';
 
 const Review = () => {
+    const [reviews, setReviews] = useReviews()
     return (
         <div>
-            <h1>This is review page</h1>
+            <h1 className='home-title2' >What Our customers Say!</h1>
+            <div className="review-cart-conatiner">
+                {
+                    reviews.map(review => <ReviewCart
+                        key={review.id}
+                        review={review}
+                    ></ReviewCart>)
+                }
+            </div>
         </div>
     );
 };
