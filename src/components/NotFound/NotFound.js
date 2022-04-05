85d18fc99a2f2953ab18de8dc019a9815ad1a346
Nargis21 <nargisakther1933@gmail.com>
2022-04-05 16:10:13 +0600

    Update readme file

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
-            <h1>Page is not Found</h1>
+        <div className='page-not-found'>
+            <img src="https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png" alt="" />
         </div>
     );
 };
