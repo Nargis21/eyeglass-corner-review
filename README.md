3f12d69b45134ad10ee784a85db493a7d7e12bbb
Nargis21 <nargisakther1933@gmail.com>
2022-04-05 16:53:22 +0600

    Update Chart

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # EYEGLASS-CORNER
 
 ## [Live Website Link](https://eyeglass-corner-review-nargis21.netlify.app/)
+- Create Route.
+- Link Navbar With Route.
+- Display Customer Review.
+- Create Line Chart and Var Chart
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>this is blogs page</h1>
+        <div className='questions'>
+            <h1>What is context API?</h1>
+            <h3></h3>
+            <h1>What is semantic tag?</h1>
+            <h3></h3>
+            <h1>What is the different between inline and inline block elements?</h1>
+
         </div>
     );
 };
