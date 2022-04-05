eec96bfed9bce1f072f4c756af8417f878d9dccb
Nargis21 <nargisakther1933@gmail.com>
2022-04-05 16:55:27 +0600

    Update

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,9 +4,7 @@ const Blogs = () => {
     return (
         <div className='questions'>
             <h1>What is context API?</h1>
-            <h3></h3>
             <h1>What is semantic tag?</h1>
-            <h3></h3>
             <h1>What is the different between inline and inline block elements?</h1>
 
         </div>
